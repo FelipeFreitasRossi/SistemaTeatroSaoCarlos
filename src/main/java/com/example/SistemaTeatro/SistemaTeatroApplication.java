d710bbccbf44c9da9678aced629ab297f829b58a
FelipeFreitasRossi <lipeferossi@gmail.com>
2026-07-29 20:16:04 -0300

    feat: Criação do Backend Spring Boot para o Teatro

---
+++ src/main/java/com/example/SistemaTeatro/SistemaTeatroApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaTeatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTeatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTeatroApplication.class, args);
+	}
+
+}
