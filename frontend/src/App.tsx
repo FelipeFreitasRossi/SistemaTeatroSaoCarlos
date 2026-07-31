55396134dabde7af54c0f57df4a395f2b503442b
FelipeFreitasRossi <lipeferossi@gmail.com>
2026-07-30 22:11:20 -0300

    Remove pacote react-vlibras-plugin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home';
@@ -10,17 +9,13 @@ function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-[#0B0B0B] text-white flex flex-col">
-        {/* VLibras - Acessibilidade para surdos */}
         <VLibras />
-
-        {/* Skip link para navegação por teclado */}
         <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[9999] focus:bg-white focus:text-black focus:px-4 focus:py-2 focus:rounded-lg focus:shadow-lg"
         >
           Pular para o conteúdo principal
         </a>
-
         <Header />
         <main id="main-content" className="flex-1">
           <Routes>
